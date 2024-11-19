f8ae7a43193db3d9fd830e99a8baa911aa3d0dbd
Atheek Hebbar <119834221+atheek2003@users.noreply.github.com>
2024-11-19 12:06:14 +0530

    Update example.js

--- docs/js/example.js
+++ docs/js/example.js
@@ -750,3 +750,75 @@ $(document).ready(function() {
   // Initialize FPP view
   fppView.init(blueprint3d);
 });
+
+/*
+ * Initialize!
+ */
+
+$(document).ready(function() {
+  // main setup
+  var opts = {
+    floorplannerElement: 'floorplanner-canvas',
+    threeElement: '#viewer',
+    threeCanvasElement: 'three-canvas',
+    textureDir: "models/textures/",
+    widget: false
+  }
+  var blueprint3d = new BP3D.Blueprint3d(opts);
+
+  var modalEffects = new ModalEffects(blueprint3d);
+  var viewerFloorplanner = new ViewerFloorplanner(blueprint3d);
+  var contextMenu = new ContextMenu(blueprint3d);
+  var sideMenu = new SideMenu(blueprint3d, viewerFloorplanner, modalEffects);
+  var textureSelector = new TextureSelector(blueprint3d, sideMenu);        
+  var cameraButtons = new CameraButtons(blueprint3d);
+  mainControls(blueprint3d);
+
+  // Initialize FPP view
+  fppView.init(blueprint3d);
+
+  // Set up FPP keyboard controls explanation
+  const keyboardControls = `
+    <div id="fpp-controls-hint" class="alert alert-info" style="display: none;">
+      <strong>FPP Controls:</strong><br>
+      W - Move forward<br>
+      S - Move backward<br>
+      A - Move left<br>
+      D - Move right<br>
+      Q - Rotate left<br>
+      E - Rotate right
+    </div>
+  `;
+  $('#viewer').append(keyboardControls);
+
+  // Show/hide controls hint when FPP is toggled
+  $('#toggle-fpp').click(function() {
+    $('#fpp-controls-hint').toggle($(this).hasClass('btn-primary'));
+  });
+
+  // Update camera buttons behavior for FPP mode
+  const originalZoomIn = CameraButtons.prototype.zoomIn;
+  const originalZoomOut = CameraButtons.prototype.zoomOut;
+  const originalPan = CameraButtons.prototype.pan;
+
+  CameraButtons.prototype.zoomIn = function(e) {
+    if (!fppView.active) {
+      originalZoomIn.call(this, e);
+    }
+  };
+
+  CameraButtons.prototype.zoomOut = function(e) {
+    if (!fppView.active) {
+      originalZoomOut.call(this, e);
+    }
+  };
+
+  CameraButtons.prototype.pan = function(direction) {
+    if (!fppView.active) {
+      originalPan.call(this, direction);
+    }
+  };
+
+  // Load a simple rectangle room
+  blueprint3d.model.loadSerialized('{"floorplan":{"corners":{"f90da5e3-9e0e-eba7-173d-eb0b071e838e":{"x":204.85099999999989,"y":289.052},"da026c08-d76a-a944-8e7b-096b752da9ed":{"x":672.2109999999999,"y":289.052},"4e3d65cb-54c0-0681-28bf-bddcc7bdb571":{"x":672.2109999999999,"y":-178.308},"71d4f128-ae80-3d58-9bd2-711c6ce6cdf2":{"x":204.85099999999989,"y":-178.308}},"walls":[{"corner1":"71d4f128-ae80-3d58-9bd2-711c6ce6cdf2","corner2":"f90da5e3-9e0e-eba7-173d-eb0b071e838e","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"f90da5e3-9e0e-eba7-173d-eb0b071e838e","corner2":"da026c08-d76a-a944-8e7b-096b752da9ed","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"da026c08-d76a-a944-8e7b-096b752da9ed","corner2":"4e3d65cb-54c0-0681-28bf-bddcc7bdb571","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}},{"corner1":"4e3d65cb-54c0-0681-28bf-bddcc7bdb571","corner2":"71d4f128-ae80-3d58-9bd2-711c6ce6cdf2","frontTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0},"backTexture":{"url":"rooms/textures/wallmap.png","stretch":true,"scale":0}}],"wallTextures":[],"floorTextures":{},"newFloorTextures":{}},"items":[]}');
+});
